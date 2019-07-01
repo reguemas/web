05e6cd5c0f80140c0058c364584a2a2e6d32a571
Gifre Masague <gifremasague@gmail.com>
2019-07-01 22:07:18 +0200

    filtres i agenda

--- src/components/activitats.vue
+++ src/components/activitats.vue
@@ -1,16 +1,16 @@
 <template>
     <div>
       <b-row class="m-0">
-        <b-col cols="12" md="5" xl="3" class="px-4 calendar d-flex flex-column">
+        <b-col cols="12" md="5" xl="3" class="calendar d-flex flex-column">
 
           <!-- Visualitzacio nomes en mobil -->
           
           <b-button :pressed.sync="myToggle" size="lg" variant="info" class="my-4 d-md-none justify-content-center">Filtres i Agenda</b-button>
           <div v-if=myToggle class="d-md-none">
-            <b-form v-if=myToggle class="mb-4 justify-content-center">
+            <b-form v-if=myToggle class="mb-4 w-50 mx-auto">
               <!-- <label class="mr-sm-3" for="Inline-Filters">Aquí Farem</label> -->
               <b-form-select
-                class=""
+                class="mt-3"
                 :value="null"
                 :options="{ '1': 'One', '2': 'Two', '3': 'Three' }"
                 id="filtreActivitat"
@@ -19,7 +19,7 @@
               </b-form-select>
 
               <b-form-select
-                class=""
+                class="mt-3"
                 :value="null"
                 :options="{ '1': 'Gener', '2': 'Febrer', '3': 'Març', '4': 'Abril', '5': 'Maig', '6': 'Juny', '7': 'Juliol', '8': 'Agost', '9': 'Setembre', '10': 'Octubre', '11': 'Novembre', '12': 'Desembre' }"
                 id="filtreMes"
@@ -28,14 +28,14 @@
               </b-form-select>
             </b-form>
             <v-calendar is-dark is-expanded></v-calendar>
-            <h4 class="mt-4 textCyan">Activitat Seleccionada</h4>
-            <card class= "my-4"/>
+            <h4 class="mt-4 mx-auto textCyan">Activitat Seleccionada</h4>
+            <card class= "my-4 mx-auto"/>
           </div>
 
           <!-- Visualitzacio en dues columnes -->
 
           <div class="d-none d-md-flex flex-column">
-          <b-form class="my-4">
+          <b-form class="my-4 w-75 mx-auto">
             <!-- <label class="mr-sm-3" for="Inline-Filters">Aquí Farem</label> -->
             <b-form-select
               class="mt-3"
@@ -54,7 +54,7 @@
               <option slot="first" :value="null">Quin Mes</option>
             </b-form-select>
           </b-form>
-          <v-calendar is-dark is-expanded></v-calendar>
+          <v-calendar is-dark is-expanded class=""></v-calendar>
           <h4 class="mt-4 mx-auto textCyan">Activitat Seleccionada</h4>
           <card class= "mt-4 mx-auto"/>
           </div>
@@ -138,6 +138,7 @@ export default {
 
   .calendar{
     background-color: #94b6aa;
+    text-align: center;
   }
 
   #buttonFilters{
