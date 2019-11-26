a2b2c2744ab5e929ae00fa4d1b5543cbfb26d8ec
Gifre Masague <gifremasague@gmail.com>
2019-11-27 00:36:56 +0100

    resolen vulnerabilitats

--- src/components/filtersCalendar.vue
+++ src/components/filtersCalendar.vue
@@ -145,7 +145,6 @@ export default {
       };
       console.log(activitatsDia);
       this.diaSeleccionat = activitatsDia;
-      console.log(this.diaSeleccionat);
       return activitatsDia;
     },
   },
