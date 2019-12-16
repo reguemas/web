8463e33cc3a452fb8b7e8b671a6cd22a4a06c9db
Aleix Masague <alemasar@gmail.com>
2019-12-16 14:30:23 +0100

    Canvis watch galeries

--- src/components/activitats.vue
+++ src/components/activitats.vue
@@ -3,94 +3,104 @@
     <b-row class="m-0 p-0">
       <b-col cols="12" md="6" lg="5" xl="4" xxl="3" class="p-4 agendaFiltres">
         <div class="d-flex justify-content-center">
-          <b-button 
+          <b-button
             :pressed.sync="myToggle"
             size="lg"
             variant="info"
             class="justify-content-center d-md-none agendaFiltresButton"
-          >
-          Filtres i Agenda
-          </b-button>
+          >Filtres i Agenda</b-button>
         </div>
-        <filtersCalendar class="visualitzacioDesktopAgenda"/>
-        <filtersCalendar v-if="myToggle==true" class="mt-4"/>
+        <filtersCalendar class="visualitzacioDesktopAgenda" />
+        <filtersCalendar v-if="myToggle==true" class="mt-4" />
       </b-col>
 
       <b-col cols="12" md="6" lg="7" xl="8" xxl="9" class="p-0 carrussel">
-
-        <b-row ref="mascaraGaleria" v-if="isMobile==false" class="m-0 ml-4 overflow-hidden position-relative">
+        <b-row
+          ref="mascaraGaleria"
+          v-if="isMobile==false"
+          class="m-0 ml-4 overflow-hidden position-relative"
+        >
           <h3 class="m-0 pt-4">Activitats {{keys[0]}}</h3>
           <gallery
-            v-if=visibilitatGaleriaEsportiva 
-            :activitatsCarousel="activitatsCarousel.Esportives" 
-            :titolGaleria="keys[0]" 
-            :ampladaPantallaActivitat="this.ampladaPantalla" 
+            v-if="visibilitatGaleriaEsportiva"
+            :activitatsCarousel="activitatsCarousel.Esportives"
+            :titolGaleria="keys[0]"
+            :ampladaPantallaActivitat="ampladaPantalla"
             class="pt-3"
           />
           <h3 class="m-0 pt-4">Activitats {{keys[1]}}</h3>
-          <gallery 
-            v-if=visibilitatGaleriaCultural 
-            :activitatsCarousel="activitatsCarousel.Culturals" 
-            :titolGaleria="keys[1]" 
-            :ampladaPantallaActivitat="this.ampladaPantalla" 
+          <gallery
+            v-if="visibilitatGaleriaCultural"
+            :activitatsCarousel="activitatsCarousel.Culturals"
+            :titolGaleria="keys[1]"
+            :ampladaPantallaActivitat="ampladaPantalla"
             class="pt-3"
           />
           <h3 class="m-0 pt-4">Activitats {{keys[2]}}</h3>
           <gallery
-            v-if=visibilitatGaleriaSocial 
-            :activitatsCarousel="activitatsCarousel.Socials" 
-            :titolGaleria="keys[2]" 
-            :ampladaPantallaActivitat="this.ampladaPantalla" 
+            v-if="visibilitatGaleriaSocial"
+            :activitatsCarousel="activitatsCarousel.Socials"
+            :titolGaleria="keys[2]"
+            :ampladaPantallaActivitat="ampladaPantalla"
             class="pt-3"
           />
         </b-row>
 
         <b-row v-if="isMobile==true" class="m-0 ml-4 position-relative">
           <h3 class="m-0 pt-4">Activitats {{keys[0]}}</h3>
-            <gallery 
-              v-if=visibilitatGaleriaEsportiva
-              :activitatsCarousel="activitatsCarousel.Esportives"
-              :ampladaPantallaActivitat="this.ampladaPantalla" 
-              class="pt-3 overflow-auto"
-            />
+          <gallery
+            v-if="visibilitatGaleriaEsportiva"
+            :activitatsCarousel="activitatsCarousel.Esportives"
+            :ampladaPantallaActivitat="ampladaPantalla"
+            class="pt-3 overflow-auto"
+          />
           <h3 class="m-0 pt-4">Activitats {{keys[1]}}</h3>
-            <gallery 
-              v-if=visibilitatGaleriaCultural 
-              :activitatsCarousel="activitatsCarousel.Culturals"  
-              :ampladaPantallaActivitat="this.ampladaPantalla" 
-              class="pt-3 overflow-auto"
-            />
+          <gallery
+            v-if="visibilitatGaleriaCultural"
+            :activitatsCarousel="activitatsCarousel.Culturals"
+            :ampladaPantallaActivitat="ampladaPantalla"
+            class="pt-3 overflow-auto"
+          />
           <h3 class="m-0 pt-4">Activitats {{keys[2]}}</h3>
-            <gallery 
-              v-if=visibilitatGaleriaSocial 
-              :activitatsCarousel="activitatsCarousel.Socials" 
-              :titolGaleria="keys[2]" 
-              :ampladaPantallaActivitat="this.ampladaPantalla"
-              class="pt-3 overflow-auto"
-            />
+          <gallery
+            v-if="visibilitatGaleriaSocial"
+            :activitatsCarousel="activitatsCarousel.Socials"
+            :titolGaleria="keys[2]"
+            :ampladaPantallaActivitat="ampladaPantalla"
+            class="pt-3 overflow-auto"
+          />
         </b-row>
 
         <b-row class="m-0 py-4 justify-content-center carrussel">
           <b-button
             size="lg"
-            href="http://ce-terrassa.cat/activitats-del-cet/" 
-            class="btnTipusActivitats tipusActivitats mr-xl-5">
-              <img src="../assets/botonsActivitats/tipusActivitats.png" class="mr-3 botonsActivitatsImg">
-              Totes les Activitats
-            </b-button>
-          <b-button
-            size="lg"
-            href="http://ce-terrassa.cat/infantilijuvenil/"
-            class="btnTipusActivitats familiaActivitats">
-              <img src="../assets/botonsActivitats/familiaActivitats.png" class="mr-3 botonsActivitatsImg">
-              Activitats pels Teus Fills i en Família
+            href="http://ce-terrassa.cat/activitats-del-cet/"
+            class="btnTipusActivitats tipusActivitats mr-xl-5"
+          >
+            <img
+              src="../assets/botonsActivitats/tipusActivitats.png"
+              class="mr-3 botonsActivitatsImg"
+            />
+            Totes les Activitats
           </b-button>
           <b-button
             size="lg"
-            href="https://ca.wikiloc.com/wikiloc/map.do?lt=41.9945&ln=2.2169&z=9&k=1&event=map&uid=2621467" 
-            class="btnTipusActivitats wikilocActivitats ml-xxl-5 mx-lg-4">
-              <img src="../assets/botonsActivitats/Wikiloc.png" class="mr-3 botonsActivitatsImg">
-              Canal CET a Wikiloc
+            href="http://ce-terrassa.cat/infantilijuvenil/"
+            class="btnTipusActivitats familiaActivitats"
+          >
+            <img
+              src="../assets/botonsActivitats/familiaActivitats.png"
+              class="mr-3 botonsActivitatsImg"
+            />
+            Activitats pels Teus Fills i en Família
+          </b-button>
+          <b-button
+            size="lg"
+            href="https://ca.wikiloc.com/wikiloc/map.do?lt=41.9945&ln=2.2169&z=9&k=1&event=map&uid=2621467"
+            class="btnTipusActivitats wikilocActivitats ml-xxl-5 mx-lg-4"
+          >
+            <img src="../assets/botonsActivitats/Wikiloc.png" class="mr-3 botonsActivitatsImg" />
+            Canal CET a Wikiloc
           </b-button>
         </b-row>
       </b-col>
@@ -99,13 +109,12 @@
 </template>
 
 <script>
-import gallery from './gallery.vue'
-import filtersCalendar from './filtersCalendar.vue'
-import totesActivitats from './json/activitatsCarousels.json'
-
+import gallery from "./gallery.vue";
+import filtersCalendar from "./filtersCalendar.vue";
+import totesActivitats from "./json/activitatsCarousels.json";
 
 export default {
-  name: 'activitats',
+  name: "activitats",
 
   components: {
     gallery,
@@ -117,151 +126,164 @@ export default {
       myToggle: false,
       activitatsCarousel: totesActivitats,
       keys: Object.keys(totesActivitats),
-      visibilitatGaleriaEsportiva:true,
-      visibilitatGaleriaCultural:true,
-      visibilitatGaleriaSocial:true,
-      isMobile:false,
-/*       ampladaPantalla:'', */
+      visibilitatGaleriaEsportiva: true,
+      visibilitatGaleriaCultural: true,
+      visibilitatGaleriaSocial: true,
+      isMobile: false,
+      amplada: 0
+    };
+  },
+  computed: {
+    ampladaPantalla: function() {
+      if (this.$refs.mascaraGaleria) {
+        return this.$refs.mascaraGaleria.offsetWidth;
+      } else {
+        return this.amplada;
+      }
     }
   },
-
+  watch:{
+    ampladaPantalla: function (){
+      console.log(this.$refs.mascaraGaleria.offsetWidth)
+      return this.$refs.mascaraGaleria.offsetWidth;
+    }
+  },
   mounted() {
-    if( navigator.userAgent.match(/Android/i)
-    || navigator.userAgent.match(/webOS/i)
-    || navigator.userAgent.match(/iPhone/i)
-    || navigator.userAgent.match(/iPad/i)
-    || navigator.userAgent.match(/iPod/i)
-    || navigator.userAgent.match(/BlackBerry/i)
-    || navigator.userAgent.match(/Windows Phone/i)) {
-      this.isMobile=true;
+    if (
+      navigator.userAgent.match(/Android/i) ||
+      navigator.userAgent.match(/webOS/i) ||
+      navigator.userAgent.match(/iPhone/i) ||
+      navigator.userAgent.match(/iPad/i) ||
+      navigator.userAgent.match(/iPod/i) ||
+      navigator.userAgent.match(/BlackBerry/i) ||
+      navigator.userAgent.match(/Windows Phone/i)
+    ) {
+      this.isMobile = true;
     }
-    if (this.activitatsCarousel.Esportives==undefined) {
-      this.visibilitatGaleriaEsportiva=false;
-    } else if (this.activitatsCarousel.Culturals==undefined) {
-      this.visibilitatGaleriaCultural=false;
-    } else if (this.activitatsCarousel.Socials==undefined) {
-      this.visibilitatGaleriaSocial=false;
+    if (this.activitatsCarousel.Esportives == undefined) {
+      this.visibilitatGaleriaEsportiva = false;
+    } else if (this.activitatsCarousel.Culturals == undefined) {
+      this.visibilitatGaleriaCultural = false;
+    } else if (this.activitatsCarousel.Socials == undefined) {
+      this.visibilitatGaleriaSocial = false;
     }
-    this.ampladaPantalla = this.$refs.mascaraGaleria.offsetWidth;
-    console.log("activitats",this.ampladaPantalla);
-  },
-}
-
+    this.amplada = this.$refs.mascaraGaleria.offsetWidth;
+    console.log("activitats", this.$refs.mascaraGaleria.offsetWidth);
+  }
+};
 </script>
 
 <style>
-  .botonsActivitatsImg{
-    width: 40px;
-    height: 40px;
-  }
+.botonsActivitatsImg {
+  width: 40px;
+  height: 40px;
+}
 </style>
 
 <style scoped>
-
-  @media (max-width: 767px) {
-    .visualitzacioDesktopAgenda{
-      display:none !important;
-    }
+@media (max-width: 767px) {
+  .visualitzacioDesktopAgenda {
+    display: none !important;
   }
+}
 
-  .agendaFiltres{
-    background:#3d4855;
+.agendaFiltres {
+  background: #3d4855;
+}
+
+.carrussel {
+  background: #afc8ad;
+}
+
+.botons {
+  background: #3d4855;
+}
+
+/* Botons totes activitats en galeries */
+
+.btnTipusActivitats {
+  border: none !important;
+  font-size: 1.2rem !important;
+  display: inline-flex !important;
+  text-align: left !important;
+  align-items: center !important;
+  justify-content: center !important;
+  color: cyan;
+}
+
+.btnTipusActivitats:hover {
+  border: none !important;
+  color: #103440;
+  font-weight: 700;
+}
+
+.btnTipusActivitats:active {
+  border: none !important;
+  color: #103440;
+  font-weight: 700;
+}
+
+.tipusActivitats {
+  background-color: #e08d4b;
+}
+
+.tipusActivitats:hover {
+  background-color: #ad7140;
+}
+
+.tipusActivitats:active {
+  background-color: #ad7140;
+}
+
+@media (max-width: 1199px) {
+  .tipusActivitats {
+    margin-bottom: 1.5rem;
+    margin-right: 3rem;
+    margin-left: 3rem;
   }
+}
 
-  .carrussel{
-    background:#afc8ad;
+.familiaActivitats {
+  background-color: #a6bf4e;
+}
+
+.familiaActivitats:hover {
+  background-color: #768639;
+}
+
+.familiaActivitats:active {
+  background-color: #768639;
+}
+
+@media (max-width: 1199px) {
+  .familiaActivitats {
+    margin-right: 3rem;
+    margin-left: 3rem;
   }
+}
 
-  .botons{
-    background:#3d4855;
+.wikilocActivitats {
+  background-color: #82c1f4;
+}
+
+.wikilocActivitats:hover {
+  background-color: #6898c0;
+}
+
+.wikilocActivitats:active {
+  background-color: #6898c0;
+}
+
+@media (max-width: 1428px) {
+  .wikilocActivitats {
+    display: none !important;
   }
+}
 
-  /* Botons totes activitats en galeries */
+/* Botons que amaga els filtres i agenda en movil */
 
-  .btnTipusActivitats {
-    border:none !important;
-    font-size: 1.2rem !important;
-    display:inline-flex !important;
-    text-align: left !important;
-    align-items: center !important;
-    justify-content:center !important;
-    color:cyan;
-  }
-
-  .btnTipusActivitats:hover {
-    border:none !important;
-    color:#103440;
-    font-weight:700;
-  }
-
-  .btnTipusActivitats:active {
-    border:none !important;
-    color:#103440;
-    font-weight:700;
-  }
-
-  .tipusActivitats{
-    background-color:#e08d4b;
-  }
-
-  .tipusActivitats:hover{
-    background-color:#ad7140;
-  }
-
-  .tipusActivitats:active{
-    background-color:#ad7140;
-  }
-
-  @media (max-width: 1199px) {
-    .tipusActivitats {
-      margin-bottom:1.5rem;
-      margin-right:3rem;
-      margin-left:3rem;
-    }
-  }
-
-  .familiaActivitats{
-    background-color:#a6bf4e;
-  }
-
-  .familiaActivitats:hover{
-    background-color:#768639;
-  }
-
-  .familiaActivitats:active{
-    background-color:#768639;
-  }
-
-  @media (max-width: 1199px) {
-    .familiaActivitats {
-      margin-right:3rem;
-      margin-left:3rem;
-    }
-  }
-
-  .wikilocActivitats{
-    background-color:#82c1f4;
-  }
-
-  .wikilocActivitats:hover{
-    background-color:#6898c0;
-  }
-
-  .wikilocActivitats:active{
-    background-color:#6898c0;
-  }
-
-  @media (max-width: 1428px) {
-    .wikilocActivitats {
-      display: none !important;
-    }
-  }
-
-  /* Botons que amaga els filtres i agenda en movil */
-
-  .agendaFiltresButton{
-    width:100%;
-    max-width: 350px;
-  }
-
+.agendaFiltresButton {
+  width: 100%;
+  max-width: 350px;
+}
 </style>
--- src/components/gallery.vue
+++ src/components/gallery.vue
@@ -2,166 +2,185 @@
   <div>
     <b-row class="m-0" @mouseover="visualitzacioBotons" @mouseleave="controlsVisibilitat=false">
       <b-list-group horizontal ref="esMou" class="esMou pr-3">
-        <b-list-group-item class="p-0 carruselActivitats" :key="index" v-for="(activitat,index) in activitatsCarousel">
-          <card :activitat="activitat"/>
+        <b-list-group-item
+          class="p-0 carruselActivitats"
+          :key="index"
+          v-for="(activitat,index) in activitatsCarousel"
+        >
+          <card :activitat="activitat" />
         </b-list-group-item>
       </b-list-group>
       <div v-if="controlsVisibilitat">
-        <button class="botoAnterior" v-if=botoAnteriorVisibilitat @click="movimentCarousel" ref="anterior">&lsaquo;</button>
-        <button class="botoSeguent" v-if=botoSeguentVisibilitat @click="movimentCarousel" ref="seguent">&rsaquo;</button>
+        <button
+          class="botoAnterior"
+          v-if="botoAnteriorVisibilitat"
+          @click="movimentCarousel"
+          ref="anterior"
+        >&lsaquo;</button>
+        <button
+          class="botoSeguent"
+          v-if="botoSeguentVisibilitat"
+          @click="movimentCarousel"
+          ref="seguent"
+        >&rsaquo;</button>
       </div>
     </b-row>
   </div>
 </template>
 
 <script>
-
-import card from './card.vue'
+import card from "./card.vue";
 
 export default {
+  name: "gallery",
 
-  name: 'gallery',
-
-  props:["activitatsCarousel","ampladaPantallaActivitat"],
+  props: ["activitatsCarousel", "ampladaPantallaActivitat"],
 
   components: {
-    card,
+    card
   },
- 
+
   data() {
     return {
-      count:0,
-      botoAnteriorVisibilitat:false,
-      botoSeguentVisibilitat:true,
-      controlsVisibilitat:false,
-      ampladaPantallaGaleria: this.ampladaPantallaActivitat,
+      count: 0,
+      botoAnteriorVisibilitat: false,
+      botoSeguentVisibilitat: true,
+      controlsVisibilitat: false,
+      ampladaPantallaGaleria: this.ampladaPantallaActivitat
     };
   },
 
   mounted() {
-    if( navigator.userAgent.match(/Android/i)
-    || navigator.userAgent.match(/webOS/i)
-    || navigator.userAgent.match(/iPhone/i)
-    || navigator.userAgent.match(/iPad/i)
-    || navigator.userAgent.match(/iPod/i)
-    || navigator.userAgent.match(/BlackBerry/i)
-    || navigator.userAgent.match(/Windows Phone/i)) {
-      this.controlsVisibilitat=false;
+    if (
+      navigator.userAgent.match(/Android/i) ||
+      navigator.userAgent.match(/webOS/i) ||
+      navigator.userAgent.match(/iPhone/i) ||
+      navigator.userAgent.match(/iPad/i) ||
+      navigator.userAgent.match(/iPod/i) ||
+      navigator.userAgent.match(/BlackBerry/i) ||
+      navigator.userAgent.match(/Windows Phone/i)
+    ) {
+      this.controlsVisibilitat = false;
     }
-    this.element = this.$refs.esMou;
-    this.longitudGaleria = this.element.childNodes.length;
-    this.$nextTick(function () {
-      this.numCardsVisibles = Math.floor((this.ampladaPantallaGaleria)/316);
-      this.numClickFinalGaleria = Math.floor(this.longitudGaleria/this.numCardsVisibles);
-      if (this.numCardsVisibles==1) {
-        this.numClickFinalGaleria--;
-      }
-      if ((this.longitudGaleria*316)<this.ampladaPantallaGaleria){
-        this.numClickFinalGaleria=0;
-        this.controlsVisibilitat=false;
-      }
-    })
   },
 
-  methods:{
+  methods: {
     visualitzacioBotons: function() {
       if (this.numClickFinalGaleria == 0) {
-        this.controlsVisibilitat=false;
+        this.controlsVisibilitat = false;
       } else {
-        this.controlsVisibilitat=true;
+        this.controlsVisibilitat = true;
       }
     },
 
     movimentCarousel: function() {
-      console.log("longitudGaleria",this.longitudGaleria);
-      console.log("numCardsVisibles",this.numCardsVisibles);
-      console.log("ampladaPantallaGaleria",this.ampladaPantallaGaleria);
+      console.log("longitudGaleria", this.longitudGaleria);
+      console.log("numCardsVisibles", this.numCardsVisibles);
+      console.log("ampladaPantallaGaleria", this.ampladaPantallaGaleria);
       console.log(this.numClickFinalGaleria);
-      if (this.numCardsVisibles==0){
+      if (this.numCardsVisibles == 0) {
         this.numCardsVisibles++;
       }
-      let distanciaMoure = (this.numCardsVisibles)*316;
+      let distanciaMoure = this.numCardsVisibles * 316;
       if (distanciaMoure == 0) {
         distanciaMoure = 316;
       }
-      if (event.target.className==="botoAnterior") {
+      if (event.target.className === "botoAnterior") {
         this.count++;
-        this.element.style.transform = "translateX("+this.count*distanciaMoure+"px)";
-        this.botoSeguentVisibilitat=true;
-        if (this.count==0) {
-          this.botoAnteriorVisibilitat=false;
+        this.element.style.transform =
+          "translateX(" + this.count * distanciaMoure + "px)";
+        this.botoSeguentVisibilitat = true;
+        if (this.count == 0) {
+          this.botoAnteriorVisibilitat = false;
         }
       }
-      if (event.target.className==="botoSeguent") {
+      if (event.target.className === "botoSeguent") {
         this.count--;
-        this.element.style.transform = "translateX("+this.count*distanciaMoure+"px)";
-        this.botoAnteriorVisibilitat=true;
+        this.element.style.transform =
+          "translateX(" + this.count * distanciaMoure + "px)";
+        this.botoAnteriorVisibilitat = true;
         if (this.count == -this.numClickFinalGaleria) {
-          this.botoSeguentVisibilitat=false;
+          this.botoSeguentVisibilitat = false;
         }
       }
-    },
+    }
   },
-}
-
+  watch: {
+    ampladaPantallaActivitat: function(newAmplada, oldAmplada) {
+      if (newAmplada != oldAmplada) {
+        this.element = this.$refs.esMou;
+        this.longitudGaleria = this.element.childNodes.length;
+        this.numCardsVisibles = Math.floor(newAmplada / 316);
+        this.numClickFinalGaleria = Math.floor(
+          this.longitudGaleria / this.numCardsVisibles
+        );
+        if (this.numCardsVisibles == 1) {
+          this.numClickFinalGaleria--;
+        }
+        if (this.longitudGaleria * 316 < newAmplada) {
+          this.numClickFinalGaleria = 0;
+          this.controlsVisibilitat = false;
+        }
+      }
+    }
+  }
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.fons {
+  background-color: red;
+}
 
-  .fons{
-    background-color:red;
-  }
+.esMou {
+  transition: transform 1s ease-in-out;
+}
 
-  .esMou{
-    transition: transform 1s ease-in-out;
-  }
+.list-group-item {
+  border: none !important;
+  margin-left: 1rem;
+}
 
-  .list-group-item{
-    border:none !important;
-    margin-left:1rem;
-  }
+.list-group-item:first-child {
+  margin-left: 0 !important;
+}
 
-  .list-group-item:first-child{
-    margin-left:0 !important;
-  }
+.botoAnterior {
+  position: absolute;
+  left: 0;
+  margin-top: 0px;
+  border: none !important;
+  color: white;
+  font-size: 50px;
+  line-height: 300px;
+  text-align: center;
+  width: 75px;
+  padding: 0 !important;
+  background: linear-gradient(
+    to right,
+    rgba(0, 0, 0, 0.8) 0%,
+    rgba(0, 0, 0, 0.6) 50%,
+    rgba(0, 0, 0, 0) 100%
+  );
+}
 
-  .botoAnterior{
-    position: absolute;
-    left:0;
-    margin-top:0px;
-    border:none !important;
-    color:white;
-    font-size: 50px;
-    line-height: 300px;
-    text-align:center;
-    width:75px;
-    padding: 0 !important;
-    background: linear-gradient(
-      to right,
-      rgba(0, 0, 0, 0.8) 0%,
-      rgba(0, 0, 0, 0.6) 50%,
-      rgba(0, 0, 0, 0.0) 100%
-    );
-  }
-
-  .botoSeguent{
-    position: absolute;
-    right:0;
-    margin-top:0px;
-    border:none !important;
-    color:white;
-    font-size: 50px;
-    line-height: 300px;
-    text-align:center;
-    width:75px;
-    padding: 0 !important;
-    background: linear-gradient(
-      to left,
-      rgba(0, 0, 0, 0.8) 0%,
-      rgba(0, 0, 0, 0.6) 50%,
-      rgba(0, 0, 0, 0.0) 100%
-    );
-  }
-
+.botoSeguent {
+  position: absolute;
+  right: 0;
+  margin-top: 0px;
+  border: none !important;
+  color: white;
+  font-size: 50px;
+  line-height: 300px;
+  text-align: center;
+  width: 75px;
+  padding: 0 !important;
+  background: linear-gradient(
+    to left,
+    rgba(0, 0, 0, 0.8) 0%,
+    rgba(0, 0, 0, 0.6) 50%,
+    rgba(0, 0, 0, 0) 100%
+  );
+}
 </style>
